48d82d68f8eb863a8dcb926a468d607944b79f64
SaharMahmoudDev <elzerossss@gmail.com>
2026-09-02 14:43:31 +0300

    feat(admin): add delete store API

--- src/modules/admin/index.ts
+++ src/modules/admin/index.ts
@@ -1,4 +1,5 @@
 export { default as adminStoreApi } from "./api/admin-store.api";
 export { default as adminStoreKeys } from "./api/admin-store.keys";
 export { default as useGetAdminStores } from "./hooks/useGetAdminStores";
-export type { AdminStoreListParams, AdminStoreListResponse } from "./types/admin-store.types";
+export { default as useDeleteStore } from "./hooks/useDeleteStore";
+export type { AdminStoreListParams, AdminStoreListResponse, DeleteStoreResponse } from "./types/admin-store.types";
